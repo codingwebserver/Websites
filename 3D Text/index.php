0ea2dc38a33b48ff911392f419a49297132e9d5d
codingapp <codingwebserver@gmail.com>
2024-07-18 10:38:46 +0200

    Update index.php

--- 3D Text/index.php
+++ 3D Text/index.php
@@ -1,22 +1,32 @@
-<?php
-session_start();
-if(!isset($_SESSION['logged']) || $_SESSION['logged'] !== true){
-    header("Location: //Login Page Link");
-    exit;
-};
-
-?>
-
 <html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'Page_Icon.ico' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+    ">
+    <link rel="stylesheet" href="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'style.css' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+    ">
     <title>3D Text</title>
 </head>
 <body>
     <span class="text">3D Text</span>
-    <script src="script.js"></script>
+    <script src="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'script.js' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+    "></script>
 </body>
 </html>
