714b915aac9e80a2c18e79762d47aa2c309649cb
codingapp <codingwebserver@gmail.com>
2024-07-18 10:33:11 +0200

    Update index.php

--- 3D Glowing Bottle/index.php
+++ 3D Glowing Bottle/index.php
@@ -2,7 +2,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'style.css' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+    ">
+    <link rel="icon" type="image/x-icon" href="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'Page_Icon.ico' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+    ">
     <title>3D Glowing Bottle</title>
 </head>
 <body>
