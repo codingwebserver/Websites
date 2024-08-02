e81dad349d8c110550a146d1370eced08f64d208
codingapp <codingwebserver@gmail.com>
2024-08-02 21:33:20 +0200

    Update and rename index.html to index.php

---
+++ Ambient Light/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'style.css' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+        ">
+    <title>Ambient Light</title>
+</head>
+<body>
+    <div class="player">
+        <video src="#Video" id="video" controls autoplay loop></video>
+        <canvas id="light"></canvas>
+    </div>
+    <div class="container">
+        <button class="amb_l_btn_a">Activate Ambient Light</button>
+        <button class="amb_l_btn_d">Deactivate Ambient Light</button>
+    </div>
+    <script src="
+        <?php 
+            $token = bin2hex(random_bytes(16));
+            $baseUrl = 'script.js' . '?token=' . $token;
+            echo $baseUrl;
+        ?>
+        "></script>
+</body>
+</html>
