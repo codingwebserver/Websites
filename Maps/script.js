58fddcdea31a402e87b1c918f17e6af835916499
codingapp <codingwebserver@gmail.com>
2024-08-04 19:18:40 +0200

    Update script.js

--- Maps/script.js
+++ Maps/script.js
@@ -1,2 +1,2 @@
 document.getElementById("send").addEventListener("click", function(){
-    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + document.getElementById("street").value +"+" + document.getElementById("city").value + "&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8")});
+    document.getElementById("map").setAttribute("src", "https://www.google.com/maps/embed/v1/place?q=" + document.getElementById("street").value +"+" + document.getElementById("city").value + "&key=API-KEY")});
