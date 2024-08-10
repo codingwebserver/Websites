35cf133de499acf55f6d9a2a7c6626cb9eb9d50d
codingapp <codingwebserver@gmail.com>
2024-08-10 16:12:30 +0200

    Update Heart.php

--- Heart/Heart.php
+++ Heart/Heart.php
@@ -1,12 +1,3 @@
-<?php
-session_start();
-if(!isset($_SESSION['logged']) || $_SESSION['logged'] !== true){
-    header("Location: https://codingapp.net/login_page");
-    exit;
-};
-
-?>
-
 <html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
@@ -14,14 +5,14 @@ if(!isset($_SESSION['logged']) || $_SESSION['logged'] !== true){
     <link rel="stylesheet" href="
         <?php 
             $token = bin2hex(random_bytes(16));
-            $baseUrl = 'https://codingapp.net/Home/Projects/Websites/Heart/style.css' . '?token=' . $token;
+            $baseUrl = 'style.css' . '?token=' . $token;
             echo $baseUrl;
         ?>
     ">
     <link rel="icon" type="image/x-icon" href="
         <?php 
             $token = bin2hex(random_bytes(16));
-            $baseUrl = 'https://codingapp.net/Sign%20Up/Logo_Black.ico' . '?token=' . $token;
+            $baseUrl = 'Logo_Black.ico' . '?token=' . $token;
             echo $baseUrl;
         ?>
     ">
@@ -32,7 +23,7 @@ if(!isset($_SESSION['logged']) || $_SESSION['logged'] !== true){
     <script src="
         <?php 
             $token = bin2hex(random_bytes(16));
-            $baseUrl = 'https://codingapp.net/Home/Projects/Websites/Heart/script.js' . '?token=' . $token;
+            $baseUrl = 'script.js' . '?token=' . $token;
             echo $baseUrl;
         ?>
     "></script>
